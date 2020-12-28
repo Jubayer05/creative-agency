8164359b3cd14a81044afe56af7bb06ab7f9d6aa
Jubayer <programmer.jubayer@gmail.com>
2020-12-29 05:16:48 +0600

    initial commit

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is the home page</h1>
+            <Header/>
+        </div>
+    );
+};
+
+export default Home;
